761c56460f3c0841c8b6f5bf3e12e7d813e85f43
Chandra <krsna.c@gmail.com>
2017-11-06 21:34:27 +0000

    again

--- req.js
+++ req.js
@@ -10,6 +10,8 @@ request.get('https://sytantris.github.io/http-examples/future.jpg')
          console.log('Response Status Code: ', response.statusCode);
          console.log('Response Status Message: ', response.statusMessage);
          console.log('Content Type: ', response.headers['content-type']);
-         console.log('Download complete.');
+       })
+       .on('end', function () {
+        console.log('Download complete.');
        })
        .pipe(fs.createWriteStream('./future.jpg'));
